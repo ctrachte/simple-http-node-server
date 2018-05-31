8497ff703433660b250427a831a4a154d8b50d81
ctrachte <cetrachte@hotmail.com>
2018-05-31 17:27:58 -0500

    add fs write function and create server function

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-
-// simple server using core HTTP
-
-const fs = require('fs');
 const http = require('http');
 const port = 3000;
+const fs = require('fs');
+
+//write a message and save it in a new .txt file
+fs.writeFile('hello-world.txt', 'Hello to this great world!', 'utf8', (err) => {
+  if (err) throw err;
+
+  console.log('success')
+});
 
 // Handles HTTP requests.
 const requestHandler = (request, response) => {
-// Writes new TXT file
-	fs.writeFile('hello-world.txt', 'Hello to this great world!', 'utf8', (err) => {
-	  if (err) throw err;
-
-	  console.log('success')
-	});
-
+  response.end(`Handling a request on port ${port}`)
+};
 
 // Create a server and pass in the  requestHandler function
 const server = http.createServer(requestHandler);
 
-// Start the server listening on port 3000
+// Start the server listening on part 8000
 server.listen(port, (err) => {
   if (err) {
     return console.log(`You have an error:  ${err}`);
