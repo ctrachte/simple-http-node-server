2d2b20514a5ce5bbd9c64efc74ffd3e30fc25a1c
ctrachte <cetrachte@hotmail.com>
2018-05-31 14:01:41 -0500

    add starter files and answers to checkpoint questions

---
+++ index.js
@@ -0,0 +1,28 @@
+
+// simple server using core HTTP
+
+const fs = require('fs');
+const http = require('http');
+const port = 3000;
+
+// Handles HTTP requests.
+const requestHandler = (request, response) => {
+// Writes new TXT file
+	fs.writeFile('hello-world.txt', 'Hello to this great world!', 'utf8', (err) => {
+	  if (err) throw err;
+
+	  console.log('success')
+	});
+
+
+// Create a server and pass in the  requestHandler function
+const server = http.createServer(requestHandler);
+
+// Start the server listening on port 3000
+server.listen(port, (err) => {
+  if (err) {
+    return console.log(`You have an error:  ${err}`);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
